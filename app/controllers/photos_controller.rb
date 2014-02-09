541db7e493fe529bb0fabf39547da1088b270e01
Jeff Horsch <jhorsch@gmail.com>
2014-02-08 21:52:36 -0600

    better color

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,7 +4,7 @@ class PhotosController < ApplicationController
   # GET /photos
   # GET /photos.json
   def index
-    @photos = Photo.where(sub_cat: 'black & white chicago - horizontal')[1..16]
+    @photos = Photo.where(sub_cat: 'black & white chicago - horizontal')[1..15]
 
   end
 
